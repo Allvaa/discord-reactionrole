9a40a5126b12028cc856c713429c3844537b5113
Allvaa <alvamailindo@gmail.com>
2020-01-09 23:32:39 +0700

    di bikin simpel aja :'v

--- src/index.js
+++ src/index.js
@@ -9,38 +9,44 @@ class ReactionRoleClient extends Client {
             console.log(`[INFO] Logged in as ${this.user.tag}.`);
             // eslint-disable-next-line no-restricted-syntax
             for (const data of Object.values(this.config.reactions)) {
-                this.channels.get(data.channelID).messages.fetch(data.messageID);
-                console.log(`[INFO] ${data.messageID} fetched.`);
+                try {
+                    this.channels.get(data.channelID).messages.fetch(data.messageID);
+                    console.log(`[INFO] ${data.messageID} fetched.`);
+                } catch (e) {
+                    console.log(`[ERR] Error when fetching ${data.messageID}, because ${e.message}.`);
+                }
             }
         });
         this.on("messageReactionAdd", (messageReaction, user) => {
-            const { message } = messageReaction;
-            const data = this.config.reactions[message.id];
-            if (messageReaction.me) {
-                return undefined;
-            }
-            if (!data || !data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]) { // eslint-disable-line
-                return undefined;
-            }
-            const member = message.guild.member(user);
-            console.log("[INFO] Role Added.");
-            return member.roles.add(data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]); // eslint-disable-line
+            this.handleRole(messageReaction, user, "add");
         });
         this.on("messageReactionRemove", (messageReaction, user) => {
-            const { message } = messageReaction;
-            const data = this.config.reactions[message.id];
-            if (messageReaction.me) {
-                return undefined;
-            }
-            if (!data || !data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]) { // eslint-disable-line
-                return undefined;
-            }
-            const member = message.guild.member(user);
-            console.log("[INFO] Role Removed.");
-            return member.roles.remove(data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]); // eslint-disable-line
+            this.handleRole(messageReaction, user, "remove");
         });
 
         this.login(this.config.token);
+    }
+
+    handleRole(messageReaction, user, type) {
+        if (!type || !["add", "remove"].includes(type)) return undefined;
+        const { message } = messageReaction;
+        const data = this.config.reactions[message.id];
+        if (messageReaction.me) return undefined;
+        if (!data || !data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]) return undefined; // eslint-disable-line
+        const member = message.guild.member(user);
+
+        switch (type) {
+        case "add":
+            member.roles.add(data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]); // eslint-disable-line
+            break;
+        case "remove":
+            member.roles.remove(data.emojis[messageReaction._emoji.id || messageReaction._emoji.name]); // eslint-disable-line
+            break;
+        default:
+            break;
+        }
+
+        return undefined;
     }
 }
 
